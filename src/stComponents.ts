dd6c64a3dea6087f2f5caff2bb6c07b16ee0511e
digiur <59296455+digiur@users.noreply.github.com>
2020-05-04 00:09:51 -0600

    git-quick-push

---
+++ src/stComponents.ts
@@ -0,0 +1,29 @@
+import * as PIXI from "pixi.js";
+import { STObject } from "./stObjects";
+
+export abstract class STComponent {
+	constructor(readonly _name: string) {}
+	abstract stUpdate(dt: number, gameObject: STObject): void;
+	abstract stDraw(dt: number, gameObject: STObject): void;
+	abstract stInit(dt: number, gameObject: STObject): void;
+}
+
+export class STSprite extends STComponent {
+	constructor(name: string, public sprite = new PIXI.Sprite()) {
+		super(name);
+	}
+
+	stInit(dt: number, gameObject: STObject): void {
+		return;
+	}
+
+	stUpdate(dt: number, gameObject: STObject): void {
+		this.sprite.position.set(gameObject._pos.x, gameObject._pos.y);
+		this.sprite.scale.set(gameObject._scale.x, gameObject._scale.y);
+		this.sprite.rotation = gameObject._rotation;
+	}
+
+	stDraw(dt: number, gameObject: STObject): void {
+		return;
+	}
+}
